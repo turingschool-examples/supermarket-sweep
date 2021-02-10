b93bda181cc307e5f517dfa3e362654b3e6be824
Alex Klick <aklick12@gmail.com>
2021-02-10 13:59:50 -0700

    ShoppingCart class passes tests, has name, capacity, products and details

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,11 +1,24 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+
+  attr_reader :name, :capacity, :products, :details
+
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
     @products = []
+    @details = Hash.new(0)
   end
+
   def change_capacity_to_int
     @capacity = @capacity.to_i
+  end
+
+  def add_product(product)
+    @products << product
+  end
+  def find_details
+    @details[:name] = @name
+    @details[:capacity] = @capacity
+    @details
   end
 end
