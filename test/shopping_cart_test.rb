e39ff3bc23d1830338feeda6dc9dd8b941be3abb
ignored-comment <info.konham@gmail.com>
2021-02-10 15:48:26 -0500

    Add setup method for shopping_cart_test to prepare for Iteration 3

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -5,33 +5,34 @@ require './lib/shopping_cart'
 
 class ShoppingCartTest < Minitest::Test
 
-  def test_it_exists
-    cart = ShoppingCart.new("King Soopers", "30items")
+  def setup
+    @cart = ShoppingCart.new("King Soopers", "30items")
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    @product4 = Product.new(:produce, 'apples', 0.99, '20')
+  end
 
-    assert_instance_of ShoppingCart, cart
+  def test_it_exists
+    assert_instance_of ShoppingCart, @cart
   end
 
   def test_it_has_readable_attributes
-    cart = ShoppingCart.new("King Soopers", "30items")
-
-    assert_equal "King Soopers", cart.name
-    assert_equal 30, cart.capacity
-    assert_equal [], cart.products
+    assert_equal "King Soopers", @cart.name
+    assert_equal 30, @cart.capacity
+    assert_equal [], @cart.products
   end
 
   def test_it_can_add_products_to_products_array
-    cart = ShoppingCart.new("King Soopers", "30items")
-    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
-    product2 = Product.new(:meat, 'chicken', 4.50, '2')
-    cart.add_product(product1)
-    cart.add_product(product2)
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
 
-    assert_equal [product1, product2], cart.products
+    assert_equal [@product1, @product2], @cart.products
   end
 
   def test_it_can_return_information_when_calling_details_method
-    cart = ShoppingCart.new("King Soopers", "30items")
     result = {name: "King Soopers", capacity: 30}
-    assert_equal result, cart.details
+    assert_equal result, @cart.details
   end
+
 end
