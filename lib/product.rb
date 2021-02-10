c615da54d0de9b1ac2434008b1eeb386aac58c12
George Ho <george.h.w.ho@gmail.com>
2021-02-10 15:07:17 -0500

    Add iteration 1

---
+++ lib/product.rb
@@ -0,0 +1,15 @@
+class Product
+  attr_reader :category, :name, :unit_price, :quantity
+
+  def initialize(category, name, unit_price, quantity)
+    @category = category
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity.to_i
+  end
+
+  def total_price
+    unit_price * quantity
+  end
+
+end
