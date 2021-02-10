8222f454867034327510377abcb8b1b0e168d22f
Kseniya <kseniya.kudelich@gmail.com>
2021-02-10 13:20:39 -0700

    Complete iteration 1

--- test/product_test.rb
+++ test/product_test.rb
@@ -2,38 +2,32 @@ require_relative 'test_helper'
 
 
 class ProductTest < Minitest::Test
-  #Iteration1
   def test_it_exists
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_instance_of Product, product
   end
 
   def test_it_has_a_category
-    skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal :paper, product.category
   end
 
   def test_it_has_a_name
-    skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 'toilet paper', product.name
   end
 
   def test_it_has_a_unit_price
-    skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 3.70, product.unit_price
   end
 
   def test_it_has_a_quantity
-    skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 10, product.quantity
   end
 
   def test_it_can_get_total_price
-    skip
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
 
