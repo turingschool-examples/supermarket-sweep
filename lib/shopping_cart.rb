50d86f89f97230458a3c4361d188ae792fe99cd7
ignored-comment <info.konham@gmail.com>
2021-02-10 15:32:50 -0500

    Assert test_...has_readable_attributes; pass

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,7 +1,14 @@
 class ShoppingCart
-
+  attr_reader :name, :products
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @products = []
   end
+
+  def capacity
+    capacity_to_integer = @capacity.delete("items").to_i
+  end
+
+
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -3,6 +3,8 @@ require 'minitest/pride'
 require './lib/product'
 require './lib/shopping_cart'
 
+# require "pry"; binding.pry
+
 class ShoppingCartTest < Minitest::Test
 
   def test_it_exists
@@ -10,4 +12,13 @@ class ShoppingCartTest < Minitest::Test
 
     assert_instance_of ShoppingCart, cart
   end
+
+  def test_it_has_readable_attributes
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "King Soopers", cart.name
+    assert_equal 30, cart.capacity
+    assert_equal [], cart.products
+  end
+
 end
