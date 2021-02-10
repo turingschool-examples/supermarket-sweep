3b82485a38b2838296a924d3a47ac5e35fbfcc2c
Aphilosopher30 <Ashafer1@fordham.edu>
2021-02-10 13:55:23 -0700

    Iteration 2: Add methods to put products into a shoping_cart

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,17 +1,16 @@
 require './lib/product'
 
 class Shopping_cart
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :products
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @products = []
   end
 
-  
-
-
-
-
+  def add_product(product)
+    @products.append(product)
+  end
 
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -6,24 +6,35 @@ require './lib/shopping_cart'
 class Shopping_cartTest < Minitest::Test
 
   def test_it_exists
-
     cart = Shopping_cart.new("King Soopers", "30items")
-
     assert_instance_of Shopping_cart, cart
   end
 
   def test_it_has_a_name
     cart = Shopping_cart.new("King Soopers", "30items")
-
     assert_equal "King Soopers", cart.name
   end
 
   def test_it_has_a_capacity
     cart = Shopping_cart.new("King Soopers", "30items")
-
     assert_equal "30items", cart.capacity
   end
 
+  def test_it_has_a_list_of_products
+    cart = Shopping_cart.new("King Soopers", "30items")
+    assert_equal [], cart.products
+  end
 
+  def test_it_can_add_to_its_list_of_products
+    cart = Shopping_cart.new("King Soopers", "30items")
+
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    assert_equal [product1, product2], cart.products
+  end
 
 end
