852cf97f2d5240f99dff25510454830332c099b1
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-02-10 13:29:46 -0700

    Add additional setup.

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -6,6 +6,9 @@ require './lib/shopping_cart'
 class ShoppingCartTest < Minitest::Test
   def setup
     @cart = ShoppingCart.new("King Soopers", "30items")
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
   end
 
   def test_it_exists_and_has_attributes
@@ -16,5 +19,11 @@ class ShoppingCartTest < Minitest::Test
 
   def test_it_starts_with_no_products
     assert_equal [], @cart.products
+  end
+
+  def test_it_can_add_products
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    assert_equal [@product1, @product2], @cart.products
   end
 end
