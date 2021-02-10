00848f8b40358e653d2a89a0060c4904af9a7216
koshea9 <keegan.oshea9@gmail.com>
2021-02-10 13:36:07 -0700

    Add Shopping Cart Class Iteration 2

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,23 @@
+class ShoppingCart
+  attr_reader :name,
+              :capacity,
+              :products
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity.to_i
+    @products = []
+  end
+
+  def add_product(product)
+    @products << product
+  end
+
+  def details
+    cart_details = {
+            :name => @name,
+            :capacity => @capacity
+          }
+        end
+
+end
