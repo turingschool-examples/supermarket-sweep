062388fdb868de71419ec660228323a1afe59004
Olivia Charles <ocharles92@gmail.com>
2021-02-10 16:14:08 -0500

    Unskipped and is now passing Sort by product quantity.

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -7,6 +7,7 @@ class ShoppingCartTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
     cart = ShoppingCart.new("King Soopers", "30items")
+
     assert_instance_of ShoppingCart, cart
     assert_equal 'King Soopers', cart.name
     assert_equal 30, cart.capacity
@@ -14,6 +15,7 @@ class ShoppingCartTest < Minitest::Test
 
   def test_it_starts_with_no_products
     cart = ShoppingCart.new("King Soopers", "30items")
+
     assert_equal [], cart.products
   end
 
@@ -21,13 +23,16 @@ class ShoppingCartTest < Minitest::Test
     cart = ShoppingCart.new("King Soopers", "30items")
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
     cart.add_product(product1)
     cart.add_product(product2)
+
     assert_equal [product1, product2], cart.products
   end
 
   def test_it_has_details
     cart = ShoppingCart.new("King Soopers", "30items")
+
     expected = {name: "King Soopers", capacity: 30}
     assert_equal expected, cart.details
   end
@@ -47,7 +52,6 @@ class ShoppingCartTest < Minitest::Test
   end
 
   def test_cart_is_is_full
-    # skip
     cart = ShoppingCart.new("King Soopers", "30items")
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
@@ -90,7 +94,7 @@ class ShoppingCartTest < Minitest::Test
   end
 
   def test_it_can_sort_by_quantity
-    skip
+
     cart = ShoppingCart.new("King Soopers", "30items")
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
