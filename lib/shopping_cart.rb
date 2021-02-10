070d0a27dc647c459362f1c374b0d3f6ffb219fa
joemecha <jwmecha@gmail.com>
2021-02-10 14:33:42 -0600

    Complete iteration 2

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,17 @@
+class ShoppingCart
+  attr_reader :name, :capacity, :products
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity.to_i
+    @products = []
+  end
+
+  def add_product(product)
+    @products << product
+  end
+
+  def details
+    {name: @name, capacity: @capacity}
+  end 
+end
---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,50 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/shopping_cart'
+require './lib/product'
+
+class ShoppingCartTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    cart = ShoppingCart.new("name", "capacity")
+
+    assert_instance_of ShoppingCart, cart
+  end
+
+  def test_cart_name
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "King Soopers", cart.name
+  end
+
+  def test_cart_capacity
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal 30, cart.capacity
+  end
+
+  def test_cart_starts_with_no_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal [], cart.products
+  end
+
+  def test_add_product
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    # assert_equal [product1, product2], cart.products
+  end
+
+  def test_cart_has_details
+    cart = ShoppingCart.new("King Soopers", "30items")
+    result = {name: "King Soopers", capacity: 30}
+
+    assert_equal result, cart.details
+  end
+
+end
