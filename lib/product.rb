91798f552106b717ec8f40bb91c63e2966ac971c
Olivia Charles <ocharles92@gmail.com>
2021-02-10 15:07:01 -0500

    Created product class, initialized the product information, and created an attr reader

---
+++ lib/product.rb
@@ -0,0 +1,13 @@
+class Product
+    attr_reader :category,
+                :name,
+                :unit_price,
+                :quantity
+
+  def initialize(category, name, unit_price, quantity)
+    @category   = category
+    @name       = name
+    @unit_price = unit_price
+    @quantity   = quantity.to_i
+  end
+end
