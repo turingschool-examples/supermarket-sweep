be98b3dcf01f864db215efa0aec3e9447d0918aa
Kseniya <kseniya.kudelich@gmail.com>
2021-02-10 14:14:30 -0700

    Add iteration 3 with 2 tests skipped

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -14,9 +14,6 @@ class ShoppingCart
   end
 
   # def details
-  #
-  #
-  #
   #   [apple, 1, banana, 2]
   #
   # {apple => 1, banana => 2}
@@ -27,5 +24,25 @@ class ShoppingCart
   #   h3: {["orange", "seedless"]=>3, "apple"=>1, "banana"=>2}
   #
   # end
+
+  def total_number_of_products
+    total_products = 0
+    @products.each do |product|
+      total_products += product.quantity
+    end
+      total_products
+  end
+
+  def is_full?
+    if @cart.total_number_of_products > @cart.capacity.split(/\D+/).to_i
+      true
+    else
+      false
+    end
+  end
+
+  def products_by_category(category)
+    @products.select { |product| product.category == category }
+  end
 
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -8,6 +8,7 @@ class ShoppingCartTest < Minitest::Test
 
     @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
   end
 
   def test_it_exists
@@ -40,7 +41,30 @@ class ShoppingCartTest < Minitest::Test
     assert_equal expected, @cart.details
   end
 
+  def test_it_can_return_total_number_of_products
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
 
+    assert_equal 13, @cart.total_number_of_products
+  end
+
+  def test_it_can_return_cart_is_full
+    skip
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    assert_equal false, @cart.is_full?
+  end
+
+  def test_it_can_return_products_in_category
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    assert_equal [@product1, @product3], @cart.products_by_category(:paper)
+  end
 
 
 end
