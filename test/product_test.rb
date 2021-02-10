d4c53fb8090cb8d4ddf1a486a5e9dd5cf599002e
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-10 15:10:58 -0500

    Complete product class testing

--- test/product_test.rb
+++ test/product_test.rb
@@ -10,31 +10,31 @@ class ProductTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
+    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal :paper, product.category
   end
 
   def test_it_has_a_name
-    skip
+    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 'toilet paper', product.name
   end
 
   def test_it_has_a_unit_price
-    skip
+    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 3.70, product.unit_price
   end
 
   def test_it_has_a_quantity
-    skip
+    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 10, product.quantity
   end
 
   def test_it_can_get_total_price
-    skip
+    # skip
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
 
