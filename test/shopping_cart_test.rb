d2bf9b46918ec70fffcc008c67b244b1f1b29fbb
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-10 15:19:03 -0500

    Create shopping_cart class tests

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/shopping_cart'
+
+class ShoppingCartTest < Minitest::Test
+
+  def test_it_exists
+  end
+
+  def test_it_has_attributes
+  end
+
+  def test_it_can_have_different_attributes
+  end
+
+end
