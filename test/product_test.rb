83b94aadbae16dbdfa349a40378eef0c72bd021b
Olivia Charles <ocharles92@gmail.com>
2021-02-10 15:11:26 -0500

    Created test for if product is hoarded

--- test/product_test.rb
+++ test/product_test.rb
@@ -41,4 +41,11 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_if_it_is_hoarded
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    assert_equal false, product1.is_hoarded?
+    assert_equal false, product2.is_hoarded?
+  end
 end
