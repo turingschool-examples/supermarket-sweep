62d43dd866858192df3aa8bf077b59b683728d38
Olivia Charles <ocharles92@gmail.com>
2021-02-10 15:56:34 -0500

    Created another assert_equal for category to ensure it works. All tests passising

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -26,9 +26,53 @@ class ShoppingCartTest < Minitest::Test
     assert_equal [product1, product2], cart.products
   end
 
-  def test_it_has_detail
+  def test_it_has_details
     cart = ShoppingCart.new("King Soopers", "30items")
     expected = {name: "King Soopers", capacity: 30}
     assert_equal expected, cart.details
+  end
+
+  def test_cart_has_products_not_full
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal 13, cart.total_number_of_products
+    assert_equal false, cart.is_full?
+  end
+
+  def test_cart_is_is_full
+    # skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    assert_equal true, cart.is_full?
+  end
+
+  def test_it_has_products_by_category
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal [product1, product3], cart.products_by_category(:paper)
+    assert_equal [product2], cart.products_by_category(:meat)
   end
 end
