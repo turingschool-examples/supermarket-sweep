1ae6e2d024b82f46195bb0f229cd150fe87d7ead
ignored-comment <info.konham@gmail.com>
2021-02-10 15:21:34 -0500

    Complete Iteration 1

--- test/product_test.rb
+++ test/product_test.rb
@@ -1,41 +1,35 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/product'
-# require "pry"; binding.pry
 
 class ProductTest < Minitest::Test
-  #Iteration1
+
   def test_it_exists
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_instance_of Product, product
   end
 
   def test_it_has_a_category
-    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal :paper, product.category
   end
 
   def test_it_has_a_name
-    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 'toilet paper', product.name
   end
 
   def test_it_has_a_unit_price
-    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 3.70, product.unit_price
   end
 
   def test_it_has_a_quantity
-    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 10, product.quantity
   end
 
   def test_it_can_get_total_price
-    # skip
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
 
@@ -54,5 +48,5 @@ class ProductTest < Minitest::Test
 
     assert_equal true, product1.hoard
   end
-  
+
 end
