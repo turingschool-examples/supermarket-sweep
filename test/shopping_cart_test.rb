c9bfbc3866b9de94270b19086f6bef87b45dc92a
Alex Klick <aklick12@gmail.com>
2021-02-10 13:59:04 -0700

    can add products and correct details tests added

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -6,6 +6,10 @@ require 'pry'
 class ShoppingTest < Minitest::Test
   def setup
     @cart = ShoppingCart.new("King Soopers", "30items")
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
     #binding.pry
   end
   def test_it_exists
@@ -18,5 +22,13 @@ class ShoppingTest < Minitest::Test
   def test_it_has_capacity
     @cart.change_capacity_to_int
     assert_equal @cart.capacity, 30
+  end
+  def test_can_add_products
+    assert_equal @cart.products, [@product1, @product2]
+  end
+  def test_has_correct_details
+    @cart.change_capacity_to_int
+    @cart.find_details
+    assert_equal @cart.details, {name: "King Soopers", capacity: 30}
   end
 end
